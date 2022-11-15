7614c302bacc398dc9eaaf3f197c963947446748
unknown <fontes.leonardo@aluno.ifsp.edu.br>
2022-11-15 19:56:18 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+    return ( 
+        <div className="App">
+            <header className="App-header">
+                <nav className="Navbar">
+                  <h1 className="App-title">Hello World!</h1>
+                </nav>
+            </header>
+        </div>
+    )
+}
+
+export default App;
